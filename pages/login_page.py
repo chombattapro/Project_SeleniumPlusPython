4fdba55dfc3d335aff5592b5ed19c62d54c7548d
VOpro <vopro@example.com>
2025-11-02 18:56:06 +0300

    Added checks for the presence of forms and URLs in LoginPage, selectors for login and registration

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,5 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -8,13 +8,11 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        current_url = self.browser.current_url
+        assert "login" in current_url, f"URL does not contain 'login': {current_url}"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present on the page"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not present on the page"
