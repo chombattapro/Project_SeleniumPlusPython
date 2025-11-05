d40296d4a5fd0cfc1f33efc2d0692e9c30a0abdd
VOpro <vopro@example.com>
2025-11-05 14:00:56 +0300

    Added new locators for checking product pages.

--- pages/locators.py
+++ pages/locators.py
@@ -7,4 +7,13 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     
+class ProductPageLocators():
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main .price_color")
+    SUCCESS_MESSAGE_PRODUCT_NAME = (By.CSS_SELECTOR, "#messages div.alert-success:nth-child(1) strong")
+    SUCCESS_MESSAGE_BASKET_PRICE = (By.CSS_SELECTOR, "#messages div.alert-info:nth-child(3) strong")
+    
+
+    
     
