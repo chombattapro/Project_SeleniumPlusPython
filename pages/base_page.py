d4179c2b69187cb16d7be071f2e04a0d2e5e3d36
VOpro <vopro@example.com>
2025-11-05 13:53:06 +0300

    Adding explicit wait for alert to appear using WebDriverWait and exception handling, fixing solve_quiz_and_get_code() call + exception handling in the absence of a second alert for reading code with alert presence check

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,17 +1,18 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoAlertPresentException
+import math
 
 class BasePage():
-    def __init__(self, browser, url):
-        self.browser = browser
-        self.url = url
-
-    def open(self):
-        self.browser.get(self.url)
-
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+
+    def open(self):
+        self.browser.get(self.url)
 
     def is_element_present(self, how, what):
         try:
@@ -19,5 +20,24 @@ class BasePage():
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        try:
+            WebDriverWait(self.browser, 10).until(EC.alert_is_present())
+            alert = self.browser.switch_to.alert
+            x = alert.text.split(" ")[2]
+            answer = str(math.log(abs((12 * math.sin(float(x))))))
+            alert.send_keys(answer)
+            alert.accept()
+            try:
+                WebDriverWait(self.browser, 5).until(EC.alert_is_present())
+                alert = self.browser.switch_to.alert
+                alert_text = alert.text
+                print(f"Your code: {alert_text}")
+                alert.accept()
+            except (NoAlertPresentException, TimeoutException):
+                print("No second alert presented")
+        except TimeoutException:
+            print("Alert did not appear within the timeout period")
 
 
