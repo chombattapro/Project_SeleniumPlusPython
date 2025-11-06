2409af6c795e8845fc9812ee68df96267fd1f6f6
VOpro <vopro@example.com>
2025-11-06 17:11:56 +0300

    Add negative tests for empty basket and BasketPage checks

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,15 @@
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 import pytest
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
+    basket_page.should_be_empty_basket_message()
 
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
