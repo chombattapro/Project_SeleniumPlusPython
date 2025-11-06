1d16477d0c5c0194dcce322a41af56169496dca9
VOpro <vopro@example.com>
2025-11-06 15:30:45 +0300

    We launched several new promotions for our online store, one of which caused an error. We detected the error and reported a failed test—XFAIL. We also rescheduled the (Test guest can see and access the login page from page X) tests.

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,18 @@
 from .pages.product_page import ProductPage
 import pytest
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
 @pytest.mark.parametrize('link', [
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
     "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -15,7 +27,6 @@ import pytest
 ])
 
 def test_guest_can_add_product_to_basket(browser, link):
-
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем драйвер и URL
     page.open()                          # открываем страницу товара
     page.add_product_to_basket()         # нажимаем кнопку добавления
