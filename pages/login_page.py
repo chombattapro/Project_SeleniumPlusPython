066c2698cfc4d6239b7b57b72bf069754e783d91
VOpro <vopro@example.com>
2025-11-07 18:13:57 +0300

    Added user registration method

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,6 +2,17 @@ from .base_page import BasePage
 from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*LoginPageLocators.REGISTER_EMAIL)
+        password_input = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD1)
+        confirm_password_input = self.browser.find_element(*LoginPageLocators.REGISTER_PASSWORD2)
+        register_button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+
+        email_input.send_keys(email)
+        password_input.send_keys(password)
+        confirm_password_input.send_keys(password)
+        register_button.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
